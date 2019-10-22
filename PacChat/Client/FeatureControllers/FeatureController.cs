c80580274489f254b084d997a7e25446be3ad157
Bach Nguyen <nlebachnlb.develop@gmail.com>
2019-10-22 10:58:16 +0700

    Cherry-pick tested MVC to dev branch, Client setup
    - Pick updated MVC from master to dev branch
    
    - Set up client prototypes and interfaces

---
+++ PacChat/Client/FeatureControllers/FeatureController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PacChat.MVC;
+
+namespace PacChat.Client.FeatureControllers
+{
+    public class FeatureController : Controller<ClientApp>
+    {
+        public Chat chat { get; private set; }
+        public AccountSetting accountSetting { get; private set; }
+        public AccountVerifier accountVerifier { get; private set; }
+
+        public FeatureController()
+        {
+            chat = new Chat();
+            accountSetting = new AccountSetting();
+            accountVerifier = new AccountVerifier();
+        }
+    }
+}
