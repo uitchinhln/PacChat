ba320deaa3959475b419d96c4c125d8893cde065
Bach Nguyen <nlebachnlb.develop@gmail.com>
2019-10-23 21:39:07 +0700

    Left side panel animation

--- PacChat/MainWindow.xaml.cs
+++ PacChat/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -21,11 +22,22 @@ namespace PacChat
     public partial class MainWindow : Window
     {
         private bool _isPanelOpened = false;
+        private List<Button> _panelButtons = new List<Button>();
+        private int _panelMaxSize, _panelMinSize;
+        private int _panelOffset;
 
         public MainWindow()
         {
             InitializeComponent();
             this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void MetaDataInit()
+        {
+            _panelMinSize = 32;
+            _panelOffset = 32;
+
+            _panelMaxSize = 128;
         }
 
         private void FormDrag(object sender, MouseEventArgs e)
@@ -36,10 +48,31 @@ namespace PacChat
             }
         }
 
+        private void ToggleLeftSidePanel()
+        {
+            var sb = this.FindResource("left-side-panel-" + (_isPanelOpened ? "compress" : "expand")) as Storyboard;
+            sb.Begin();
+            _isPanelOpened = !_isPanelOpened;
+        }
+
         private void BtnNoti_Click(object sender, RoutedEventArgs e)
         {
-            this.LeftSidePanel.Width = _isPanelOpened ? 32 : 128;
-            _isPanelOpened = !_isPanelOpened;
+            ToggleLeftSidePanel();
+        }
+
+        private void Btn3_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleLeftSidePanel();
+        }
+
+        private void BtnQuit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleLeftSidePanel();
         }
     }
 }
