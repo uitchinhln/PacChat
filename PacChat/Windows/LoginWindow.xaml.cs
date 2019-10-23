498606aa9657966bdd182d99d5997e9233ac10b9
uitchinhln <43503957+uitchinhln@users.noreply.github.com>
2019-10-24 02:16:16 +0700

    Create Login UI

---
+++ PacChat/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PacChat.Windows
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDrag(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                try
+                {
+                    this.DragMove();
+                } catch (Exception) { }
+            }
+        }
+
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void TabClick(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+            GridCursor.Margin = new Thickness(66 + (56 * index), 23, 0, 0);
+
+            if (trnsEr.SelectedIndex != index)
+            {
+                trnsEr.SelectedIndex = index;
+            }
+        }
+    }
+}
