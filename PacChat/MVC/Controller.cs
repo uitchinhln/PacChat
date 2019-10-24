18d216ca7797bf1dc5eb32164ef5f99103eea771
uitchinhln <43503957+uitchinhln@users.noreply.github.com>
2019-10-24 11:16:50 +0700

    Merge from UI

---
+++ PacChat/MVC/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacChat.MVC
+{
+    public class Controller : Entity
+    {
+
+    }
+
+    public class Controller<T> : Controller where T : App
+    {
+        new public T app
+        {
+            get
+            {
+                return (T)base.app;
+            }
+        }
+    }
+}
