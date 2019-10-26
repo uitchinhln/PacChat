9ebb3f96e74c591f35585fac5341c6164e1fceb5
Nguyen Nhat Long <nhtlonga@gmail.com>
2019-10-27 02:54:33 +0700

    Working on bubble chat

---
+++ PacChat/Resources/CustomControls/BubbleChat.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PacChat.Resources.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for BubbleChat.xaml
+    /// </summary>
+    public partial class BubbleChat : UserControl
+    {
+        public BubbleChat()
+        {
+            InitializeComponent();
+        }
+    }
+}
