e13bbc20367395292c18adbf7bb66e3a2d27177d
uitchinhln <43503957+uitchinhln@users.noreply.github.com>
2019-10-28 08:50:46 +0700

    Revert "Revert "Change startup type""
    This reverts commit 21f0eb2b52c205d23150af61717dcde818060a63.

---
+++ PacChatServer/Network/Protocol/HandShakeProtocol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacChatServer.Network.Protocol
+{
+    public class HandShakeProtocol : PacChatProtocol
+    {
+        public HandShakeProtocol() : base("HandShake")
+        {
+
+        }
+    }
+}
