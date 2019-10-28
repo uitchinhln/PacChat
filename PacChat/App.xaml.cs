db969d3c10e4e3c05bb708623881d7929549d7cb
uitchinhln <43503957+uitchinhln@users.noreply.github.com>
2019-10-28 08:50:14 +0700

    Change startup type Working on server

--- PacChat/App.xaml.cs
+++ PacChat/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PacChat.Windows;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,11 @@ namespace PacChat
     /// </summary>
     public partial class App : Application
     {
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+        }
     }
 }
