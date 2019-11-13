71563ad8fec3f9d929613e4adf35771c6c24f916
uitchinhln <43503957+uitchinhln@users.noreply.github.com>
2019-11-13 23:35:40 +0700

    Remove ChromiumWebBrowser

--- PacChat/App.xaml.cs
+++ PacChat/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using CefSharp;
-using CefSharp.Wpf;
-using PacChat.Windows.Login;
+﻿using PacChat.Windows.Login;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -19,15 +17,6 @@ namespace PacChat
     /// </summary>
     public partial class App : Application
     {
-        public App()
-        {
-            //Add Custom assembly resolver
-            AppDomain.CurrentDomain.AssemblyResolve += Resolver;
-
-            //Any CefSharp references have to be in another method with NonInlining
-            // attribute so the assembly rolver has time to do it's thing.
-            InitializeCefSharp();
-        }
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
@@ -35,39 +24,6 @@ namespace PacChat
             login.Show();
             //MainWindow main = new MainWindow();
             //main.Show();
-        }
-
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        private static void InitializeCefSharp()
-        {
-            var settings = new CefSettings();
-
-            // Set BrowserSubProcessPath based on app bitness at runtime
-            settings.BrowserSubprocessPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
-                                                   Environment.Is64BitProcess ? "x64" : "x86",
-                                                   "CefSharp.BrowserSubprocess.exe");
-
-            // Make sure you set performDependencyCheck false
-            Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
-        }
-
-        // Will attempt to load missing assembly from either x86 or x64 subdir
-        // Required by CefSharp to load the unmanaged dependencies when running using AnyCPU
-        private static Assembly Resolver(object sender, ResolveEventArgs args)
-        {
-            if (args.Name.StartsWith("CefSharp"))
-            {
-                string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
-                string archSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
-                                                       Environment.Is64BitProcess ? "x64" : "x86",
-                                                       assemblyName);
-
-                return File.Exists(archSpecificPath)
-                           ? Assembly.LoadFile(archSpecificPath)
-                           : null;
-            }
-
-            return null;
         }
     }
 }
