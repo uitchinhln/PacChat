b7f8c514aa41341161b87be592c82e4c2eb54b6d
uitchinhln <43503957+uitchinhln@users.noreply.github.com>
2019-11-25 22:02:05 +0700

    Add Get Friend's IDs packet

---
+++ PacChatServer/Network/Packets/AfterLogin/DataPreparing/FriendsListRequest.cs
@@ -0,0 +1,42 @@
+﻿using CNetwork;
+using CNetwork.Sessions;
+using DotNetty.Buffers;
+using PacChatServer.Entity.EntityProperty;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacChatServer.Network.Packets.AfterLogin.DataPreparing
+{
+    public class FriendsListRequest : IPacket
+    {
+        public void Decode(IByteBuffer buffer)
+        {
+            
+        }
+
+        public IByteBuffer Encode(IByteBuffer byteBuf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(ISession session)
+        {
+            ChatSession chatSession = session as ChatSession;
+
+            FriendsListResponse response = new FriendsListResponse();
+
+            foreach(KeyValuePair<Guid, Guid> pair in chatSession.Owner.Relationship)
+            {
+                if (Relation.Get(pair.Value).RelationType == Relation.Type.Friend)
+                {
+                    response.Friends.Add(pair.Key.ToString());
+                }
+            }
+
+            chatSession.Send(response);
+        }
+    }
+}
