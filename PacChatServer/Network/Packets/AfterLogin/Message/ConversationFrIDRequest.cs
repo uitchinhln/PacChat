0a8bd06cfd9e6cc8cd1e4af85c4f156298d37c95
uitchinhln <43503957+uitchinhln@users.noreply.github.com>
2019-11-26 02:58:21 +0700

    Working on packet

---
+++ PacChatServer/Network/Packets/AfterLogin/Message/ConversationFrIDRequest.cs
@@ -0,0 +1,34 @@
+﻿using CNetwork;
+using CNetwork.Sessions;
+using CNetwork.Utils;
+using DotNetty.Buffers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacChatServer.Network.Packets.AfterLogin.Message
+{
+    public class ConversationFrIDRequest : IPacket
+    {
+        public Guid ConversationID { get; set; }
+
+        public void Decode(IByteBuffer buffer)
+        {
+            ConversationID = Guid.Parse(ByteBufUtils.ReadUTF8(buffer));
+        }
+
+        public IByteBuffer Encode(IByteBuffer byteBuf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(ISession session)
+        {
+            ChatSession chatSession = session as ChatSession;
+
+
+        }
+    }
+}
---
+++ PacChatServer/Network/Packets/AfterLogin/Message/ConversationFrIDResponse.cs
@@ -0,0 +1,29 @@
+﻿using CNetwork;
+using CNetwork.Sessions;
+using DotNetty.Buffers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacChatServer.Network.Packets.AfterLogin.Message
+{
+    public class ConversationFrIDResponse : IPacket
+    {
+        public void Decode(IByteBuffer buffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IByteBuffer Encode(IByteBuffer byteBuf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(ISession session)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
