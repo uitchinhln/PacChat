266f493fe7ee34ae412c04a5a147743734dd5a3d
ChinhLN <ngocchinhlqd@gmail.com>
2019-12-19 09:30:37 +0700

    Bypass UIT Firewall

--- PacChat/Network/RestAPI/StreamAPI.cs
+++ PacChat/Network/RestAPI/StreamAPI.cs
@@ -11,23 +11,25 @@ namespace PacChat.Network.RestAPI
 {
     public static class StreamAPI
     {
-        private static readonly String MediaStreamUrl = "http://{0}:1403/api/message/media/{1}/{2}/{3}";
-        private static readonly String MediaThumbnailUrl = "http://{0}:1403/api/message/media/thumbnail/{1}/{2}/{3}";
+        private static readonly String MediaStreamUrl = "http://{0}:{1}/api/message/media/{2}/{3}/{4}";
+        private static readonly String MediaThumbnailUrl = "http://{0}:{1}/api/message/media/thumbnail/{2}/{3}/{4}";
 
         public static String GetMediaURL(String fileID, String conversationID)
         {
             String address = ChatConnection.Instance.WebHost;
+            int port = ChatConnection.Instance.WebPort;
             String token = ChatConnection.Instance.Session.SessionID;
 
-            return String.Format(MediaStreamUrl, address, fileID, conversationID, token);
+            return String.Format(MediaStreamUrl, address, port, fileID, conversationID, token);
         }
 
         public static String GetMediaThumbnailURL(String fileID, String conversationID)
         {
             String address = ChatConnection.Instance.WebHost;
+            int port = ChatConnection.Instance.WebPort;
             String token = ChatConnection.Instance.Session.SessionID;
 
-            return String.Format(MediaThumbnailUrl, address, fileID, conversationID, token);
+            return String.Format(MediaThumbnailUrl, address, port, fileID, conversationID, token);
         }
     }
 }
